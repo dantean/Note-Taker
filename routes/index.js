eaa8f81f0dd7d4988b15f24fbc91c8ad865075e4
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-06 00:07:19 -0400

    updated routes js based on Phil's recordings

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router=require("express").Router()
 const notes=require("./notes")
-//http://localhost:3001/api/notes
-router.use("/notes",notes)
 
-module.exports=router
+//http://localhost:3001/notes
+
+router.use("/notes",notes)
+module.exports = router
--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,57 @@
-const router=require("express").Router()
+const router = require("express").Router()
 const fs = require("fs")
-//http://localhost:3001/api/notes/
-router.get("/",(req,res)=>{
-    fs.readFile("./db/db.json","utf8",(err,data)=>{
-        const newData=JSON.parse(data)
+const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+// http://localhost:3001/notes/
+router.get("/", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        const newData = JSON.parse(data)
         res.json(newData)
     })
 })
 
-router.post("/api/notes",(req,res)=>{
+router.post("/", (req, res) => {
+    console.info(`${req.method} request received to a new note`)
+    const { title, text} = req.body
 
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        }
+
+        readAndAppend(newNote, "./db/db.json")
+    
+        const response = {
+            status: 'success',
+            body: newNote,
+          }
+      
+        res.json(response)
+    }else {
+        res.error("Error in adding note")
+    }
 })
 
-module.exports=router
+router.delete("/:id/", (req, res) => {
+
+    fs.readFile('./db/db.json', (err, data) => {
+        obj = JSON.parse(data)
+       
+        obj = obj.filter((item) => {
+            let url = req.params.id; 
+            return item.id !== url;
+        })
+       
+        console.log(obj)
+        let json = JSON.stringify(obj);
+        fs.writeFile('./db/db.json', json, (err)=>{
+            if(err) throw err
+            res.json(obj)
+        })
+    })
+})
+
+module.exports = router
